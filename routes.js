2e91238f4c4b5926faeed9a5505a3c182fb06564
Stephanie McIlroy <stephanie.mcilroy@gmail.com>
2017-08-08 14:22:02 -0400

    postgres set up

--- routes.js
+++ routes.js
@@ -3,16 +3,6 @@ var route = express.Router();
 var pg = require('pg');
 var pool = require('./pg-connection-pool');
 var request = require('request');
-// var Client = require('node-rest-client').Client;
-//
-// var client = new Client();
-//
-// var args = {
-//     path: 'https://developer.nps.gov/api/v0/alerts?parkCode=yell,yose'
-//     headers: {
-//       "Autorization": "'0B0717D2-1A05-4930-8D18-F9417FB2713D'"
-//       "Accept": "text/json"
-//   };
 
 var options = {
   url:'https://developer.nps.gov/api/v0/parks?limit=10&fields=images,name,addresses,contacts',
