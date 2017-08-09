28a0f4f4f29c8d1e93f35c22f6afdbcd205d3c5f
Stephanie McIlroy <stephanie.mcilroy@gmail.com>
2017-08-09 13:29:33 -0400

    Test

--- public/js/parksController.js
+++ public/js/parksController.js
@@ -9,8 +9,8 @@ $scope.submitPark=function(park){
   console.log(park.name);
   console.log(park.addresses[0].line1)
   // $('#parkButton')on('click', function(){
-    $('.buttonExp').show("slow").append('<h1>' + park.name+ '</h1></p>' + park.addresses[0].line1 +'</p></p>' + park.addresses[0].city + ', ' + park.addresses[0].stateCode + ' ' + park.addresses[0].postalCode +'</p>');
-  }
+  //   $('.buttonExp').show("slow").append('<h1>' + park.name+ '</h1></p>' + park.addresses[0].line1 +'</p></p>' + park.addresses[0].city + ', ' + park.addresses[0].stateCode + ' ' + park.addresses[0].postalCode +'</p>');
+  // }
 $scope.hidePark=function(){
   $('.buttonExp').hide("slow").text('').removeClass('hidePark');
   $('buttonExp').addClass('hidePark');
