dd817d1d96cbad8f24e4f006f73e766f8cfa67d1
jonkrause <jonathonkrause@gmail.com>
2017-08-14 09:23:20 -0400

    home changes

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -8,6 +8,7 @@ app.controller('homeCtrl', function ($scope, $location, $timeout, parkFactory) {
 
     parkFactory.setParks().then(function () {
         $scope.newPark = parkFactory.getParks();
+//        console.log('newPark ' + $scope.newPark)
     });
 
     parkFactory.setEvents().then(function () {
@@ -41,34 +42,38 @@ app.controller('homeCtrl', function ($scope, $location, $timeout, parkFactory) {
     // alerts ticker - timeout set to 5 seconds to allow api call
 
     $timeout(function () {
+        
+        // declaring variables
         var tickerItems = parkFactory.getAlerts()
-
-        var tickerIndex = 0;
+        var tickerIndex = 0; // start index for ticker
         var tickerDuration = 5; // time in seconds
-        var ticker;
+        var ticker; // declaring ticker variable so it can be refrenced in the function
 
+        // function to go through array and print out each .category property of the array. 'ticker.html' is replacing the current html on the DOM with the provided html.
         var setTicker = function () {
             ticker.html("<a href='" + tickerItems[tickerIndex].url + "'>" + tickerItems[tickerIndex++].title + "</a>" + "<br><span class='tickerCat'>(<b>" + tickerItems[tickerIndex].category + "</b>)</span>");
             if (tickerIndex >= tickerItems.length) {
                 tickerIndex = 0;
             }
         };
-
+        
+        // function to make the ticker fade in/out
         var rotateTicker = function () {
             ticker.fadeOut('slow', function () {
                 setTicker();
                 ticker.fadeIn('slow');
             });
         };
-
+        
+        // targeting the array where the ticker will be placed
         ticker = $('span#ticker');
         setTicker();
         if (tickerItems.length > 1) {
-            window.setInterval(rotateTicker, tickerDuration * 1000);
+            window.setInterval(rotateTicker, tickerDuration * 1000); // this is setting the tickerDuration variable to multiply by 1000ms. allowing us to change the speed the ticker scrolls through in seconds with the variable above.
         }
 
 
-    }, 5000);
+    }, 5000); // five second timeout to allow time for the api to load and feed ticker information.
 
 
 
